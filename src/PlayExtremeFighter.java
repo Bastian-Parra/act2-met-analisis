6dc27abf37872161484304db8f3145d31ec7001c
bstydev <bastian.parra@alumnos.uv.cl>
2024-05-07 18:35:13 -0400

    1

--- src/PlayExtremeFighter.java
+++
@@ -1,9 +0,0 @@
-public class PlayExtremeFighter {
-    public static void main(String[] args) {
-        luchador jugador = new Guerrero(100);  // 100 es la vida inicial
-        luchador enemigo = new Mago(80);  // 80 es la vida inicial del enemigo
-
-        FightEngine motorDeCombate = new FightEngine(jugador, enemigo);
-        motorDeCombate.iniciarCombate();
-    }
-}
