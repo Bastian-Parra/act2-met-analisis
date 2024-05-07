ffdab606937db1b90f244dbca3becb133ad0881c
bstydev <83727399+Bastian-Parra@users.noreply.github.com>
2024-05-07 13:18:00 -0400

    Archivos base de la actividad 2.

---
+++ src/ChoroPortenho.java
@@ -0,0 +1,21 @@
+
+public class ChoroPortenho {
+	
+	public String golpear(){
+		return("ALETAZO MARINO!!!");
+	}
+	
+	public String patear(){
+		return("PATADA DE LA JAIBAAAAA!!!");
+	}
+
+	public String saltar(){
+		return("PIQUERO INFERNAAAAAL!!!");
+	}
+	
+	public String LanzarPoder(){
+		return("SHIAAAAAAAAAAAA!!!");
+	}
+	
+	
+}
---
+++ src/MineroWarrior.java
@@ -0,0 +1,20 @@
+
+public class MineroWarrior {
+	public String golpear(){
+		return("PU„O DEL PIRQUINEROOOO!!!");
+	}
+	
+	public String patear(){
+		return("PATADA DEL CATEADOOOOOOR!!!");
+	}
+
+	public String saltar(){
+		return("SALTO EXPLOSIVOOOOO!!!");
+	}
+	
+	public String LanzarPoder(){
+		return("EXPLOSION A TAJO ABIERTOOOOOO!!!");
+	}
+	
+
+}
